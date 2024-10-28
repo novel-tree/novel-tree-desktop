c440026f5565ccd75de38b4645b595a960b3e333
Michael Kenji Wilkins <kenji.wilkins@gmail.com>
2024-10-29 07:48:24 +0900

    Add icons

--- electron/main.ts
+++ electron/main.ts
@@ -31,7 +31,7 @@ let win: BrowserWindow | null;
 
 function createWindow() {
   win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
+    icon: path.join(process.env.VITE_PUBLIC, "novel-tree.svg"),
     webPreferences: {
       preload: path.join(__dirname, "preload.mjs"),
     },
