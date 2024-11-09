813cf618a12caadc4e84efa6faa6a71ecccf3211
Michael Kenji Wilkins <kenji.wilkins@gmail.com>
2024-11-09 23:39:04 +0900

    re-prettier

--- src/components/Contents/ContentHeaderBar/ContentHeaderBar.tsx
+++ src/components/Contents/ContentHeaderBar/ContentHeaderBar.tsx
@@ -8,7 +8,7 @@ export const ContentHeaderBar: FC<ContentHeaderBarProps> = ({
   children,
 }: ContentHeaderBarProps) => {
   return (
-    <div className="p-4 border-b flex justify-between items-center">
+    <div className="flex items-center justify-between border-b p-4">
       {children}
     </div>
   );
